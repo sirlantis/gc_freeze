dc3f10d64938905b5372b9bd2409d8ce06b18e53
Marcel Jackwerth <marceljackwerth@gmail.com>
2021-06-16 10:50:21 +0200

    Use python_requires insteasd of assert
    This should prevent a crash on `python setup.py egg_info`.

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ from setuptools import Extension
 from setuptools import setup
 
 
-assert platform.python_implementation() == 'CPython', 'Not compatible with non-CPython implementations.'
-assert sys.version_info < (3, 7), 'Not compatible with CPython 3.7 and above.'
-
-
 gc_freeze = Extension(
     name=str('gc_freeze'),
     sources=[str('src/init.c'), str('src/freeze.c'), str('src/util.c')],
@@ -24,6 +20,7 @@ setup(
     name=str('gc-freeze'),
     version=str('1.1.0'),
     description=str('Garbage collection freezing packaged as an extension for versions of Python before 3.7.'),
+    python_requires=str('<3.8'),
     platforms=str('all'),
     ext_modules=[gc_freeze],
 )
